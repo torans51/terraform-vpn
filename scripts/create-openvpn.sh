bfef82869b83d9599e0759efcf3e04a3e15e4209
torans51 <>
2021-10-08 17:12:10 +0200

    Update script to create the stack

--- scripts/create-openvpn.sh
+++ scripts/create-openvpn.sh
@@ -3,8 +3,10 @@ set -e
 
 ssh-keygen -f generated/ssh/openvpn_ssh_key -t rsa -b 4096 -q -N ""
 
-if [ ! -f "backend.tfvars" ]; then echo "Missing terraform backend config file"; fi
-terraform init -backend-config=backend.tfvars
-
 if [ ! -f "aws_shared_credentials" ]; then echo "Missing terraform aws credentials file"; fi
-AWS_SHARED_CREDENTIALS_FILE=aws_shared_credentials terraform apply -auto-approve
+export AWS_SHARED_CREDENTIALS_FILE=aws_shared_credentials
+
+if [ ! -f "backend.tfvars" ]; then echo "Missing terraform backend config file"; fi
+terraform init -backend-config=backend.tfvars -migrate-state
+
+terraform apply -auto-approve
